81ade874407acf00914ad0df01d165d8a9b695a4
xxf1ow <thinkwd@foxmail.com>
2026-07-28 14:50:07 +0800

    test: register robot navigation pytest suite

--- core/navigation/robot_navigation/setup.py
+++ core/navigation/robot_navigation/setup.py
@@ -17,6 +17,7 @@ setup(
         (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
+    tests_require=['pytest'],
     zip_safe=True,
     maintainer='xxf1ow',
     maintainer_email='20twenty.degree@gmail.com',
